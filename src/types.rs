dfcc0fb3e1e2bda7e46c74bdc8bc8deafe9b0f07
Promise Odelola <102539296+PromiseJoseph@users.noreply.github.com>
2026-09-01 23:51:29 +0100

    Add HttpResponse struct to types.rs

--- src/types.rs
+++ src/types.rs
@@ -13,6 +13,8 @@ pub struct HttpRequest {
     pub headers: HashMap<String, String>,
     pub body: String,
 }
+
+#[derive(Debug, Clone)]
 pub struct HttpResponse {
     pub status_code: u16,
     pub headers: HashMap<String, String>,
